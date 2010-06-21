ef22a733b89323f76858783aaea0620901b81d28
Robert Rees <spam@shudderfix.com>
2010-06-21 22:26:56 +0100

    Updated moods

--- src/fiction_generator/data.clj
+++ src/fiction_generator/data.clj
@@ -3,5 +3,6 @@
 (def story-elements
 	{:title-start ["Chrono" "Aero"]
 	:title-end ["mech" "punk" "noiac" "(o)poli" "naut"]
-	:moods ["neo-noir" "alternate-history" "ancient" "dystopian" "shrill"]
-	:locations ["America" "Japan" "Soviet Russia" "Victorian Britain" "medieval Europe" "Aztec Empire" "Atlantis" "terraformed Mars" "Antarctica" "one-way spaceflight" "Outer Rim World" "post-apocalypse highways"]})
+	:moods ["neo-noir" "alternate-history" "ancient" "dystopian" "shrill"
+		"VR-simulated" "metaphorical" "anachronistic" "leather-clad" "coal-powered" "dragon-filled"]
+	:locations ["America" "Japan" "Soviet Russia" "Victorian Britain" "medieval Europe" "Aztec Empire" "Atlantis" "terraformed Mars" "Antarctica" "one-way spaceflight" "Outer Rim World" "road system"]})
