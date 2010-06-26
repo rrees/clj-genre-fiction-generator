c3102275f883f02efdb91d0ae93f80759ccb8850
Robert Rees <rrees@Roberts-MacBook-Pro.local>
2010-06-26 15:35:41 +0100

    Robert and Rustem: Functional pronoun generator (at LJC2)

--- src/fiction_generator/text.clj
+++ src/fiction_generator/text.clj
@@ -1,9 +1,7 @@
 (ns fiction-generator.text)
 
 (defn a [text]
-	(let [vowels [\a \e \i \o \u \A \E \I \O \U]
-		first-character (. text charAt 0)]
-	(if (empty? (filter #(. % equals first-character) vowels)) 
-		(str "a " text)
-		(str "an " text))))
+	(let
+		[pronoun (if (re-seq #"(?i)[aeiou]" (str (first text)))  "an " "a ")]
+	(str pronoun text)))
 
